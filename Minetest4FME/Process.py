cc138ec408c6a27b2776e23127137cb61cb3fd47
Tino <t.miegel@conterra.de>
2014-09-09 09:39:14 +0200

    initial commit

---
+++ Minetest4FME/Process.py
@@ -0,0 +1,150 @@
+'''
+Created on 08.09.2014
+
+@author: Tino Miegel (t.miegel@conterra.de)
+'''
+import fmeobjects, zlib, array, cStringIO, os, fme, re
+import colors
+
+def readU8(f):
+    return ord(f.read(1))
+
+def readU16(f):
+    return ord(f.read(1)) * 256 + ord(f.read(1))
+
+def readU32(f):
+    return ord(f.read(1)) * 256 * 256 * 256 + ord(f.read(1)) * 256 * 256 + ord(f.read(1)) * 256 + ord(f.read(1))
+
+def unsignedToSigned(i, max_positive):
+    if i < max_positive:
+        return i
+    else:
+        return i - 2 * max_positive
+        
+def getIntegerAsBlock(i):
+    x = unsignedToSigned(i % 4096, 2048)
+    i = int((i - x) / 4096)
+    y = unsignedToSigned(i % 4096, 2048)
+    i = int((i - y) / 4096)
+    z = unsignedToSigned(i % 4096, 2048)
+    return x, y, z
+    
+def readS32(f):
+    return unsignedToSigned(ord(f.read(1)) * 256 * 256 * 256 + ord(f.read(1)) * 256 * 256 + ord(f.read(1)) * 256 + ord(f.read(1)), 2 ** 31)
+        
+def content_is_ignore(d):
+    return d in ["ignore"]
+
+def content_is_air(d):
+    return d in [126, 127, 254, "air"]
+
+unknown_node_names = []
+unknown_node_ids = []
+
+apps = {}
+
+def buildApps():
+    lib = fmeobjects.FMELibrary()    
+    for c in colors.colors.keys():   
+        a = fmeobjects.FMEAppearance()
+        a.setName(unicode(c))
+        a.setColorAmbient(float(colors.colors[c][0]) / 255, float(colors.colors[c][1]) / 255, float(colors.colors[c][2]) / 255)
+        key = lib.addAppearance(a)
+        apps[c] = key
+
+# Template Class Interface:
+class FeatureProcessor(object):
+    def __init__(self):
+        buildApps()
+        pass
+    def input(self, feature):
+        packer = zlib.decompressobj()
+        data = feature.getAttribute("data")
+        f = cStringIO.StringIO(data[4:])
+        decomp = array.array("B", packer.decompress(f.read()))
+        # print(decomp)
+        f.close()
+        f = cStringIO.StringIO(packer.unused_data)
+        # data = packer.unused_data
+        packer = zlib.decompressobj()
+        meta = array.array("B", packer.decompress(f.read()))
+        f.close()
+        f = cStringIO.StringIO(packer.unused_data)
+        static_object_version = readU8(f)
+        static_object_count = readU16(f)
+        for i in range(0, static_object_count):
+            # u8 type (object type-id)
+            object_type = readU8(f)
+            # s32 pos_x_nodes * 10000
+            pos_x_nodes = readS32(f) / 10000
+            # s32 pos_y_nodes * 10000
+            pos_y_nodes = readS32(f) / 10000
+            # s32 pos_z_nodes * 10000
+            pos_z_nodes = readS32(f) / 10000
+            # u16 data_size
+            data_size = readU16(f)
+            # u8[data_size] data
+            data = f.read(data_size)
+        
+        timestamp = readU32(f)
+        # print("* timestamp="+str(timestamp))
+        
+        id_to_name = {}
+        name_id_mapping_version = readU8(f)
+        num_name_id_mappings = readU16(f)
+        # print("* num_name_id_mappings: "+str(num_name_id_mappings))
+        for i in range(0, num_name_id_mappings):
+            node_id = readU16(f)
+            name_len = readU16(f)
+            name = f.read(name_len)
+            # print(str(node_id)+" = "+name)
+            id_to_name[node_id] = name
+        # feature.setAttribute("test",id_to_name)
+        # feature.setAttribute("timestamp",timestamp)
+        # feature.setAttribute("decomp",decomp)
+        # feature.setAttribute("version",data[0])
+        # feature.setAttribute("flags",data[1])
+        # feature.setAttribute("content width",data[2])
+        # feature.setAttribute("param width",data[3])
+        f.close()
+        x, y, z = getIntegerAsBlock(feature.getAttribute("pos"))
+        for i in xrange(16):
+            for j in xrange(16):
+                for k in xrange(16):
+                    datapos = i + j * 16 + k * 256
+                    content = decomp[datapos * 2] << 8 | decomp[datapos * 2 + 1]
+                    try:
+                        content = id_to_name[content]
+                        #print content 
+                    except KeyError:
+                        pass
+                    if content_is_ignore(content):
+                        pass
+                    elif content_is_air(content):
+                        pass
+                    else:
+                        f = fmeobjects.FMEFeature()
+                        box = fmeobjects.FMEBox((x * 16 + i, y * 16 + j, z * 16 + k, x * 16 + i + 1, y * 16 + j + 1, z * 16 + k + 1))
+                        f.setAttribute("content", content)
+                        try:
+                            box.setAppearanceReference (apps[content], True)
+                        except KeyError:
+                            fmeobjects.FMELogFile().logMessageString(content, fmeobjects.FME_WARN)
+                        f.setGeometry(box)
+                        f.setAttribute("pos",feature.getAttribute("pos"))
+                        self.pyoutput(f)
+                      
+                    
+        # Check flags
+        # is_underground = (data[1] & 1) != 0
+        # day_night_differs = (data[1] & 2) != 0
+        # lighting_expired = (data[1] & 4) != 0
+        # generated = (data[1] & 8) != 0
+        # print("is_underground="+str(is_underground))
+        # print("day_night_differs="+str(day_night_differs))
+        # print("lighting_expired="+str(lighting_expired))
+        # print("generated="+str(generated))
+        # print("Laenge {}".format(len(data)))
+        
+    def close(self):
+        pass
---
+++ Minetest4FME/colors.py
@@ -0,0 +1,78 @@
+colors = { \
+	"0" : (128,128,128),  # CONTENT_STONE \
+	"2" : (9,6,6), # CONTENT_WATER) \
+	"3" : (5,5,0), # CONTENT_TORCH) \
+	"9" : (9,6,6), # CONTENT_WATERSOURCE) \
+	"e" : (7,6,1), # CONTENT_SIGN_WALL) \
+	"f" : (8,9,0), # CONTENT_CHEST) \
+	"10" : (8,8,8), # CONTENT_FURNACE) \
+	"15" : (3,8,2), # CONTENT_FENCE) \
+	"1e" : (2,9,3), # CONTENT_RAIL) \
+	"1f" : (4,0,0), # CONTENT_LADDER) \
+	"20" : (5,0,0), # CONTENT_LAVA) \
+	"21" : (5,0,0), # CONTENT_LAVASOURCE) \
+	"800" : (7,4,1), # CONTENT_GRASS) \
+	"801" : (6,8,1), # CONTENT_TREE) \
+	"802" : (8,5,8), # CONTENT_LEAVES) \
+	"803" : (2,9,8), # CONTENT_GRASS_FOOTSTEPS) \
+	"804" : (8,8,0), # CONTENT_MESE) \
+	"805" : (1,4,6), # CONTENT_MUD) \
+	"808" : (4,8,2), # CONTENT_WOOD) \
+	"809" : (0,4,6), # CONTENT_SAND) \
+	"80a" : (3,3,3), # CONTENT_COBBLE) \
+	"80b" : (9,9,9), # CONTENT_STEEL) \
+	"80c" : (3,3,2), # CONTENT_GLASS) \
+	"80d" : (9,2,8), # CONTENT_MOSSYCOBBLE) \
+	"80e" : (0,0,0), # CONTENT_GRAVEL) \
+	"80f" : (4,0,0), # CONTENT_SANDSTONE) \
+	"810" : (0,5,0), # CONTENT_CACTUS) \
+	"811" : (0,0,5), # CONTENT_BRICK) \
+	"812" : (4,8,2), # CONTENT_CLAY) \
+	"813" : (8,5,8), # CONTENT_PAPYRUS) \
+	"814" : (6,0,0), # CONTENT_BOOKSHELF) \
+	"815" : (5,0,1), # CONTENT_JUNGLETREE) \
+	"816" : (2,1,5), # CONTENT_JUNGLEGRASS) \
+	"817" : (5,3,5), # CONTENT_NC) \
+	"818" : (2,0,5), # CONTENT_NC_RB) \
+	"819" : (0,0,0), # CONTENT_APPLE) \
+	"default:stone" : (128,128,128), \
+	"default:stone_with_coal" : (50,50,50), \
+	"default:water_flowing" : (39,66,106), \
+	"default:torch" : (255,255,0), \
+	"default:water_source" : (39,66,106), \
+	"default:sign_wall" : (117,86,41), \
+	"default:chest" : (128,79,0), \
+	"default:furnace" : (118,118,118), \
+	"default:fence_wood" : (103,78,42), \
+	"default:rail" : (162,119,53), \
+	"default:ladder" : (154,110,40), \
+	"default:lava_flowing" : (255,100,0), \
+	"default:lava_source" : (255,100,0), \
+	"default:dirt_with_grass" : (107,134,51), \
+	"default:tree" : (86,58,31), \
+	"default:leaves" : (48,95,8), \
+	"default:dirt_with_grass_and_footsteps" : (102,129,38), \
+	"default:mese" : (178,178,0), \
+	"default:dirt" : (101,84,36), \
+	"default:wood" : (104,78,42), \
+	"default:sand" : (210,194,156), \
+	"default:cobble" : (123,123,123), \
+	"default:steelblock" : (199,199,199), \
+	"default:glass" : (183,183,222), \
+	"default:mossycobble" : (219,202,178), \
+	"default:gravel" : (70,70,70), \
+	"default:sandstone" : (204,0,0), \
+	"default:cactus" : (0,215,0), \
+	"default:brick" : (170,50,25), \
+	"default:clay" : (104,78,42), \
+	"default:papyrus" : (58,105,18), \
+	"default:bookshelf" : (196,160,0), \
+	"default:jungletree" : (205,190,121), \
+	"default:junglegrass" : (62,101,25), \
+	"default:nyancat" : (255,153,255), \
+	"default:nyancat_rainbow" : (102,50,255), \
+	"default:apple" : (200,0,0), \
+	"default:desert_sand" : (210,180,50), \
+	"default:desert_stone" : (150,100,30), \
+	"default:dry_shrub" : (100,80,40), \
+}
